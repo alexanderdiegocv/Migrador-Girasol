1da13382457d88e1f53e5c1b42f6493072afb180
alexanderdiego2001@outlook.com <[tu@email.com](mailto:tu@email.com)>
2024-05-10 18:58:09 -0500

    add chnage

--- src/main/main.controller.ts
+++ src/main/main.controller.ts
@@ -1,17 +1,30 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { MainService } from './main.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Controller('users')
 export class MainController {
   constructor(private readonly mainService: MainService) {}
 
   @Get()
-  async getAllUsersFirmeasy() {
+  async getAllUsersGirasol() {
     const users = await this.mainService.getAllUsersGirasol();
 
     const sanitizedUsers = users.map(user => ({
       ...user,
       id: Number(user.id),
+      plan_id: 1,
+      rol_id: 1, 
+      tipo_documento_id: Number(user.tipodocumento_id),
+      tipouser_id: Number(user.tipouser_id),  
+      numero_documento: user.document_number,
+      nombres: user.name,
+      celular: user.phone,
+      email: user.email,
+      clave_secreta: '123456',
+      codigo_afiliacion: user.codigo_afiliacion,
+      password: user.password,
+      token: uuidv4(),
     }));
 
     return sanitizedUsers;
