c1df8801a1279a01e60e78c840148a69dcedb19d
alexanderdiego2001@outlook.com <[tu@email.com](mailto:tu@email.com)>
2024-05-13 18:56:00 -0500

    add change

--- src/main/main.controller.ts
+++ src/main/main.controller.ts
@@ -1,32 +1,48 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get,Response } from '@nestjs/common';
 import { MainService } from './main.service';
 import { v4 as uuidv4 } from 'uuid';
+import { json } from 'stream/consumers';
 
-@Controller('users')
+@Controller()
 export class MainController {
   constructor(private readonly mainService: MainService) {}
 
-  @Get()
+  @Get('users')
   async getAllUsersGirasol() {
-    const users = await this.mainService.getAllUsersGirasol();
 
-    const sanitizedUsers = users.map(user => ({
-      ...user,
-      id: Number(user.id),
-      plan_id: 1,
-      rol_id: 1, 
-      tipo_documento_id: Number(user.tipodocumento_id),
-      tipouser_id: Number(user.tipouser_id),  
-      numero_documento: user.document_number,
-      nombres: user.name,
-      celular: user.phone,
-      email: user.email,
-      clave_secreta: '123456',
-      codigo_afiliacion: user.codigo_afiliacion,
-      password: user.password,
-      token: uuidv4(),
-    }));
+    try {
 
-    return sanitizedUsers;
+      const users = await this.mainService.getAllUsersGirasol();
+
+      const sanitizedUsers = users.map(user => ({
+        id: Number(user.id),
+        plan_id: 1,
+        rol_id: (user.tipouser_id === 1) ? 1 : (user.tipouser_id === 10) ? 3 : 2,
+        tipo_documento_id: Number(user.tipodocumento_id),
+        tipouser_id: (user.tipouser_id === 10) ? 7 : user.tipouser_id,
+        numero_documento: user.document_number,
+        nombres: user.name,
+        celular: user.phone,
+        email: user.email,
+        clave_secreta: '123456',
+        codigo_afiliacion: user.codigo_afiliacion,
+        password: user.password,
+        token: uuidv4(),
+        tipo: (user.tipouser_id === 1 || user.tipouser_id === 10) ? 'I' : 'E',
+        operador_registro: (user.tipouser_id === 10) ? 'SI' : 'NO',
+        activo: (user.activo === 'SI') ? 'S' : 'N',
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      }));
+
+      return {
+        'cantidad' : sanitizedUsers.filter(user => user.tipouser_id === 5).length,
+        'usuarios' : sanitizedUsers.filter(user => user.tipouser_id === 5)
+      }
+
+    } catch (error) {
+      return Response.caller(error);
+    }
+
   }
 }
